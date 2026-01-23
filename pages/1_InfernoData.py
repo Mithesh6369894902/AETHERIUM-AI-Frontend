94e20791ea1aa4cf2d996f89f0a7af1d583d3208
D.MITHESH <114052780+Mithesh6369894902@users.noreply.github.com>
2026-01-23 11:49:00 +0530

    Update 1_InfernoData.py

--- pages/1_InfernoData.py
+++ pages/1_InfernoData.py
@@ -78,7 +78,7 @@ elif mode == "Dataset Trimmer":
 # ---------------- CLASSIFICATION ---------------- #
 elif mode == "Classification":
     file = st.file_uploader("Upload CSV", type=["csv"])
-    target = st.text_input(Target Column)
+    target = st.text_input("Target Column")
 
     if file and target and st.button("Run Classification"):
         try:
@@ -100,7 +100,7 @@ elif mode == "Classification":
 # ---------------- REGRESSION ---------------- #
 elif mode == "Regression":
     file = st.file_uploader("Upload CSV", type=["csv"])
-    target = st.text_input(Target Column)
+    target = st.text_input("Target Column")
 
     if file and target and st.button("Run Regression"):
         try:
